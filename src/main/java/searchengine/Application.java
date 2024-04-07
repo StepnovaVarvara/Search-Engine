e819831d2a3219f27ba8d925b2e8f474e679930a
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-04-07 22:49:19 +0300

    main: add all features

--- src/main/java/searchengine/Application.java
+++ src/main/java/searchengine/Application.java
@@ -2,13 +2,10 @@ package searchengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import searchengine.variables.FJP;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(Application.class, args);
-        FJP fjp = context.getBean(FJP.class);
+        SpringApplication.run(Application.class, args);
     }
 }
